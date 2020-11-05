721e9f9537a41bba67eda1d98b8e39f9401b8c3e
DIego Garabaya Rodríguez <diegogr@informatica.cifpaviles.pa>
2020-11-05 17:27:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dospantallas/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dospantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Intent ActivityDos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void abrirVentana(View view){
+        Intent intent=new Intent(this.ActivityDos.class);
+        startActivity(intent);
+    }
+}
